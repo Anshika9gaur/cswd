41d00a34ec635ed34aeac297946f8a0dfa8951e6
NIKHIL PUNDIR <145194274+nikhilpundir108@users.noreply.github.com>
2025-06-25 21:52:47 +0530

    remove about us heading

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='bg-gradient-to-r from-black to-gray-900'>
-    <h2 className=" mt-2 text-4xl text-white text-center font-bold inline-block w-full">
-      UNCOVER OUR JOURNEY
-    </h2>
+   
     <MeteorsDemo/>
     </div>
   )
