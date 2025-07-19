a2204858c18164c13df6a141c14776c5a18263c3
NIKHIL PUNDIR <145194274+nikhilpundir108@users.noreply.github.com>
2025-07-19 23:00:59 +0530

    about us

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 40 },
